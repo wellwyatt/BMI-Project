251252ed3f16ece0f3e523c5be83cab9c9bccecb
wyatt <wyatt.schlicher.2021@student.lhschools.org>
2019-02-05 08:12:50 -0500

    Set up some of my Strings and added my Scanner.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,23 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
+
     public static void main(String[] args) {
-	
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        String inches, feet;
+
+
+
+
+
+
+
+
+
+
     }
 }
