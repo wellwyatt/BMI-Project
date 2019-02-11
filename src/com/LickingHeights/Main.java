bd9e0cbc0133792a46155ade2231242bbc26097c
wyatt <wyatt.schlicher.2021@student.lhschools.org>
2019-02-11 07:58:47 -0500

    Finished my BMI Project.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,7 +8,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner keyboard;
         keyboard = new Scanner(System.in);
-        String inches, feet;
+        double userInches, userFeet, userWeight;
+        //BMI = kg/m*m
+        System.out.println("What is your height? (in feet)");
+        userFeet = keyboard.nextInt();
+
+
+        System.out.println("What is your height? (in inches)");
+        userInches = keyboard.nextInt();
+               userInches =  (userFeet * 12 + userInches);
+
+        System.out.println("What is your weight? (in lbs)");
+        userWeight = keyboard.nextInt();
+
+
+        System.out.println("Your BMI is ");
+        double BMI = (userWeight * 703) / (userInches * userInches);
+        System.out.println
+                (BMI);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
