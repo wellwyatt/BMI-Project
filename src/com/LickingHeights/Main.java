19dbece7a39693770cf582e851dac93f14dc7882
wyatt <wyatt.schlicher.2021@student.lhschools.org>
2019-02-14 08:31:32 -0500

    Added A New Feature to My BMI Project.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -10,13 +10,19 @@ public class Main {
         keyboard = new Scanner(System.in);
         double userInches, userFeet, userWeight;
         //BMI = kg/m*m
+        double lowNormal, highNormal,
+                highBMI;
+
+        lowNormal = 18.4;
+        highNormal = 24.9;
+        highBMI = 25;
         System.out.println("What is your height? (in feet)");
         userFeet = keyboard.nextInt();
 
 
         System.out.println("What is your height? (in inches)");
         userInches = keyboard.nextInt();
-               userInches =  (userFeet * 12 + userInches);
+        userInches = (userFeet * 12 + userInches);
 
         System.out.println("What is your weight? (in lbs)");
         userWeight = keyboard.nextInt();
@@ -27,28 +33,14 @@ public class Main {
         System.out.println
                 (BMI);
 
+        if (BMI >= lowNormal && BMI < highNormal)
+            System.out.println(" Your BMI is Normal.");
 
+        else if (BMI < lowNormal)
+            System.out.println(" Your BMI is low");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        else if (BMI>highNormal)
+            System.out.println (" Your BMI is High.");
 
     }
 }
